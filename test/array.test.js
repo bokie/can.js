9056250e71ca1b174a2646deeb4211630bd516f5
ボキよい <yyybokie@gmail.com>
2018-06-02 09:29:58 +0800

    Delete array.test.js

--- test/array.test.js
+++
@@ -1,9 +0,0 @@
-// import { arrUnique } from '../scripts/array'
-const arrUnique = require('../scripts/array')
-
-let arr = [1, 2, 3, 3]
-let result = [1, 3]
-
-test('unique array test', () => {
-	expect(arrUnique(arr)).toEqual(result);
-});
