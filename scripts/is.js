161ea0402117df42cac3a66e639eaa457a080a2e
bokie <yyybokie@gmail.com>
2018-06-03 23:11:37 +0800

    REFACTOR: .

--- scripts/is.js
+++ scripts/is.js
@@ -3,42 +3,41 @@
 */
 
 /**
- * getType -查询变量类型
+ * getType - 查询变量类型
+ *
  * @return {string}
 */
 function getType (param) {
-	if (param === null) {
-		return 'null'
-	}
-	let type = typeof(param)
-	if (type !== 'object') {
-		return type
-	}
+    if (param === null) {
+        return 'null'
+    }
+
+    const type = typeof(param)
+    if (type !== 'object') {
+        return type
+    }
+
     // eg: '[object Array]'' => 'array'
-	let objType = toString.call(param).slice(8, -1).toLowerCase
-	return objType
+    const objType = toString.call(param).slice(8, -1).toLowerCase
+    return objType
 }
 
 function isArray (param) {
-	return (
-		Array.isArray
-	    ?
-	    Array.isArray(param)
-	    :
-	    (getType(param) === 'array')
-	)
+    return (
+        Array.isArray ? Array.isArray(param) : (getType(param) === 'array')
+    )
 }
 
 function isString (param) {
-	return (getType(param) === 'string')
+    return (getType(param) === 'string')
 }
 
 function isObject (param) {
-	return (getType(param) === 'object')
+    return (getType(param) === 'object')
 }
 
 function isTrue (param) {
-	return (param == true)
+    return (param == true)
 }
 
-export { isString, isArray, isObject }
+export { isString, isArray, isObject, isTrue }
