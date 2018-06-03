e6b9c03f25af95f28811b9cb53f45655616a8386
bokie <yyybokie@gmail.com>
2018-06-03 23:08:57 +0800

    UPDATE: update the lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,81 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    'extends': 'eslint:recommended',
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true,
+        'mocha': true,
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 6,
+        'sourceType': 'module'
     },
-    "rules": {
-        "indent": [
-            "error",
+    'rules': {
+        'indent': [
+            'error',
             4
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
+        'linebreak-style': [
+            'error',
+            'unix'
         ],
-        "quotes": [
-            "error",
-            "double"
+        'quotes': [
+            'error',
+            'single'
         ],
-        "semi": [
-            "error",
-            "always"
-        ]
+        'semi': [
+            'error',
+            'never'
+        ],
+
+        // ECMAScript 6 rules
+
+        // require braces around arrow function bodies
+        'arrow-body-style': ['error', 'as-needed', {
+            'requireReturnForObjectLiteral': false
+        }],
+
+        // require parentheses around arrow function arguments
+        'arrow-parens': ['error', 'always'],
+
+        // enforce consistent spacing before and after the arrow in arrow functions
+        'arrow-spacing': ['error', {
+            'after': true,
+            'before': true
+        }],
+
+        'no-var': ['error'],
+
+        'no-new-symbol': ['error'],
+
+        // disallow renaming import, export, and destructured assignments to the same name
+        'no-useless-rename': ['error'],
+
+        'no-duplicate-imports': ['error'],
+
+        // disallow duplicate class members
+        'no-dupe-class-members': ['error'],
+
+        'object-shorthand': ['error', 'always', {
+            'avoidQuotes': true,
+            'ignoreConstructors': false
+        }],
+
+        'prefer-arrow-callback': ['error', {
+            'allowNamedFunctions': false,
+            'allowUnboundThis': true
+        }],
+
+        // require const declarations for variables that are never reassigned after declared
+        'prefer-const': ['error'],
+
+        // require rest parameters instead of arguments
+        'prefer-rest-params': ['error'],
+
+        // require template literals instead of string concatenation
+        'prefer-template': ['error'],
+
+        'template-curly-spacing': ['error', 'never'],
     }
-};
+}
